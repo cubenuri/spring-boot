53415845a6ea90382a43c35e57fad6f089cc159d
cubenuri <cubenuri@gmail.com>
2018-07-13 22:11:16 +0900

    spring boot 프로젝트 생성

---
+++ spring-boot-jpa-mvc/src/main/java/com/cubenuri/SpringBootJpaMvcApplication.java
@@ -0,0 +1,12 @@
+package com.cubenuri;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootJpaMvcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootJpaMvcApplication.class, args);
+	}
+}
